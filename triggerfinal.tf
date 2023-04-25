90833ba4c3cb08ac61b1154fb5c671cf547640bd
adadhore <107457695+adadhore@users.noreply.github.com>
2023-04-24 21:50:56 -0700

    Update triggerfinal.tf

--- triggerfinal.tf
+++ triggerfinal.tf
@@ -1,44 +1,87 @@
-  variable "api_key" {
+terraform {
+    required_providers {
+        harness = {
+            source = "harness/harness"
+        }
+    }
+}
+
+
+variable "api_key" {
     description = "The API key for the Harness.io API."
     default     = "pat.ROodF2xySYuWhp1rNwZuRw.64466d41ca3ee013a381a878.KiA1UfW1IRTdlEcSuZOD"
   }
   
-  variable "account_id" {
+ variable "account_id" {
     description = "The account identifier for the Harness.io account."
     default     = "ROodF2xySYuWhp1rNwZuRw"
   }
   
-  variable "org_id" {
-    description = "The organization identifier for the Harness.io organization."
-    default     = "default"
-  }
-  
+
   variable "project_id" {
     description = "The project identifier for the Harness.io project."
     default     = "testing"
   }
-  
+
+   variable "org_id" {
+    description = "The organization identifier for the Harness.io organization."
+    default     = "default"
+  }
+
   variable "pipeline_id" {
     description = "The pipeline identifier for the Harness.io pipeline."
     default     = "testpipeline"
   }
-  
-  variable "trigger_id" {
+
+
+ variable "trigger_id" {
     description = "The trigger identifier for the Harness.io trigger."
-    default     = "Webhook"
+    default     = "Webhook2"
   }
-  
+ variable "HARNESS_ACCOUNT_ID" {
+    default     = "ROodF2xySYuWhp1rNwZuRw"
+
+ }
+
+//   keep it as is
   variable "sample_key" {
     description = "The sample key to include in the webhook payload."
     default = "sample_value"
   }
 
-module "multi-stage-pipeline" {
-  // Configuration for the multi-stage-pipeline module
+// module "multi-stage-pipeline" {
+//   // Configuration for the multi-stage-pipeline module
+// }
+
+
+resource "harness_platform_triggers" "example" {
+    identifier = "identifier"
+    org_id     = "${var.org_id}"
+    project_id = "${var.project_id}"
+    name       = "Webhook2"
+    target_id  = "${var.pipeline_id}"
+    yaml       = <<-EOT
+
+    trigger:
+  name: Webhook2
+  identifier: Webhook
+  enabled: true
+  orgIdentifier: default
+  projectIdentifier: testing
+  pipelineIdentifier: testpipeline
+  source:
+    type: Webhook
+    spec:
+      type: Custom
+  inputYaml: |
+    pipeline: {}
+
+
+    EOT
 }
 
 resource "null_resource" "trigger_webhook" {
-  depends_on = [module.multi-stage-pipeline]
+//   depends_on = [module.multi-stage-pipeline]
 
   triggers = {
     payload = jsonencode({
