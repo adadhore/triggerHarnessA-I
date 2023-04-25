5b86296c1994ce0a8794a0ae57a6932cc399354a
adadhore <107457695+adadhore@users.noreply.github.com>
2023-04-25 00:23:09 -0700

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,38 @@
+variable "api_key" {
+    description = "The API key for the Harness.io API."
+    default     = "pat.ROodF2xySYuWhp1rNwZuRw.64466d41ca3ee013a381a878.KiA1UfW1IRTdlEcSuZOD"
+  }
+  
+  variable "account_id" {
+    description = "The account identifier for the Harness.io account."
+    default     = "ROodF2xySYuWhp1rNwZuRw"
+  }
+  
+  variable "project_id" {
+    description = "The project identifier for the Harness.io project."
+    default     = "testing"
+  }
+  
+  variable "org_id" {
+    description = "The organization identifier for the Harness.io organization."
+    default     = "default"
+  }
+  
+  variable "pipeline_id" {
+    description = "The pipeline identifier for the Harness.io pipeline."
+    default     = "testpipeline"
+  }
+  
+  variable "trigger_id" {
+    description = "The trigger identifier for the Harness.io trigger."
+    default     = "Webhook2"
+  }
+  
+  variable "sample_key" {
+    description = "The sample key to include in the webhook payload."
+    default     = "sample_value"
+  }
+  
+  variable "HARNESS_ACCOUNT_ID" {
+    default     = "ROodF2xySYuWhp1rNwZuRw"
+  }
