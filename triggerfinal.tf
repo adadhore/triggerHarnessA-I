5755a3b3e8a724803d31a6f6c91e13ec7633cccc
adadhore <107457695+adadhore@users.noreply.github.com>
2023-04-24 20:46:11 -0700

    Update triggerfinal.tf

--- triggerfinal.tf
+++ triggerfinal.tf
@@ -1,31 +1,37 @@
-variable "api_key" {
-  description = "The API key for the Harness.io API."
-}
-
-variable "account_identifier" {
-  description = "The account identifier for the Harness.io account."
-}
-
-variable "org_identifier" {
-  description = "The organization identifier for the Harness.io organization."
-}
-
-variable "project_identifier" {
-  description = "The project identifier for the Harness.io project."
-}
-
-variable "pipeline_identifier" {
-  description = "The pipeline identifier for the Harness.io pipeline."
-}
-
-variable "trigger_identifier" {
-  description = "The trigger identifier for the Harness.io trigger."
-}
-
-variable "sample_key" {
-  description = "The sample key to include in the webhook payload."
-  default = "sample_value"
-}
+  variable "api_key" {
+    description = "The API key for the Harness.io API."
+    default     = "pat.ROodF2xySYuWhp1rNwZuRw.64466d41ca3ee013a381a878.KiA1UfW1IRTdlEcSuZOD"
+  }
+  
+  variable "account_id" {
+    description = "The account identifier for the Harness.io account."
+    default     = "ROodF2xySYuWhp1rNwZuRw"
+  }
+  
+  variable "org_id" {
+    description = "The organization identifier for the Harness.io organization."
+    default     = "default"
+  }
+  
+  variable "project_id" {
+    description = "The project identifier for the Harness.io project."
+    default     = "testing"
+  }
+  
+  variable "pipeline_id" {
+    description = "The pipeline identifier for the Harness.io pipeline."
+    default     = "testpipeline"
+  }
+  
+  variable "trigger_id" {
+    description = "The trigger identifier for the Harness.io trigger."
+    default     = "Webhook"
+  }
+  
+  variable "sample_key" {
+    description = "The sample key to include in the webhook payload."
+    default = "sample_value"
+  }
 
 module "multi-stage-pipeline" {
   // Configuration for the multi-stage-pipeline module
@@ -45,7 +51,7 @@ resource "null_resource" "trigger_webhook" {
       curl -X POST \
         -H "content-type: application/json" \
         -H "X-Api-Key: ${var.api_key}" \
-        --url "https://app.harness.io/gateway/pipeline/api/webhook/custom/v2?accountIdentifier=${var.account_identifier}&orgIdentifier=${var.org_identifier}&projectIdentifier=${var.project_identifier}&pipelineIdentifier=${var.pipeline_identifier}&triggerIdentifier=${var.trigger_identifier}" \
+        --url "https://app.harness.io/gateway/pipeline/api/webhook/custom/v2?accountIdentifier=${var.account_id}&orgIdentifier=${var.org_id}&projectIdentifier=${var.project_id}&pipelineIdentifier=${var.pipeline_id}&triggerIdentifier=${var.trigger_id}" \
         -d '${jsonencode({
           sample_key = var.sample_key
         })}'
