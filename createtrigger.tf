29b52a5766497a4ada40e7b26fc492007b3a0409
adadhore <107457695+adadhore@users.noreply.github.com>
2023-04-25 19:49:42 -0700

    Update createtrigger.tf

--- createtrigger.tf
+++ createtrigger.tf
@@ -1,22 +1,22 @@
- resource "harness_platform_triggers" "example" {
-    identifier = ${var.pipeline_name}
-    org_id     = var.org_id
-    project_id = var.project_id
-    name       = var.pipeline_name
-    target_id  = var.pipeline_id
-    yaml       = <<-EOT
-      trigger:
-        name: ${var.pipeline_name}
-        identifier: ${var.pipeline_name}
-        enabled: true
-        orgIdentifier: ${var.org_id}
-        projectIdentifier: ${var.project_id}
-        pipelineIdentifier: ${var.pipeline_id}
-        source:
-          type: Webhook
-          spec:
-            type: Custom
-        inputYaml: |
-          pipeline: {}
-    EOT
-  }
+resource "harness_platform_triggers" "trigger" {
+  identifier = module.multi-stage-pipelines.pipeline_name
+  org_id     = var.org_id
+  project_id = var.project_id
+  name       = module.multi-stage-pipelines.pipeline_name
+  target_id  = var.pipeline_id
+  yaml       = <<-EOT
+    trigger:
+      name: "${module.multi-stage-pipelines.pipeline_name}"
+      identifier: "${module.multi-stage-pipelines.pipeline_name}"
+      enabled: true
+      orgIdentifier: ${var.org_id}
+      projectIdentifier: ${var.project_id}
+      pipelineIdentifier: ${var.pipeline_id}
+      source:
+        type: Webhook
+        spec:
+          type: Custom
+      inputYaml: |
+        pipeline: {}
+  EOT
+}
